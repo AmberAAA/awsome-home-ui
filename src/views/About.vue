75bfeabf3206435d2035ac6d924a05dd964c7879
anborong <anbr@novastar.tech>
2021-11-12 11:32:32 +0800

    test git hook

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
+<script lang="ts" setup>
+import { ref } from "@vue/reactivity";
+
+const count = ref<number>(0);
+
+const handleAdd = () => count.value++;
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 @click="handleAdd">This is an about page. {{ count }}</h1>
   </div>
 </template>
